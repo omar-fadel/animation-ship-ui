173016497e720a2ed55aeec7b0afdd3de8cfda4c
omar-fadel <omar.fadel@vodafone.com>
2024-10-26 15:09:08 +0300

    initialize library

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,8 @@
+interface ButtonProps {
+  label: string;
+}
+const Button: React.FC<ButtonProps> = ({ label }) => {
+  return <button>{label}</button>;
+};
+
+export default Button;
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
+
+// https://vite.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "src/main.ts",
+      name: "animation-ship",
+      fileName: (format) => `animation-ship-ui.${format}.js`,
+      formats: ["es"],
+    },
+    sourcemap: true,
+    // emptyOutDir: true,
+    rollupOptions: {
+      external: [],
+    },
+  },
+  plugins: [react(), dts()],
+});
