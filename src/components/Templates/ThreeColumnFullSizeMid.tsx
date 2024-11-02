01dc948892097fef9f84d8acc97c5cda193e4f3b
omar-fadel <omar.fadel@vodafone.com>
2024-11-02 14:46:40 +0200

    Refactor ThreeColumnFullSizeMid component to use VariableContainerComponent

--- src/components/Templates/ThreeColumnFullSizeMid.tsx
+++ src/components/Templates/ThreeColumnFullSizeMid.tsx
@@ -1,32 +1,66 @@
-import styled from 'styled-components';
-
-const ThreeColumnFullSizeMidComponent = styled.div`
-  display: grid;
-  grid-template-columns: repeat(10, minmax(0, 1fr));
-`;
+import VariableContainerComponent from '@atoms/VariableContainerComponent';
+import clsx from 'clsx';
+import { ContainerVariant } from 'src/types/ContainerVariant';
 
 export interface ThreeColumnFullSizeMidProps {
   startComponent: React.ReactNode;
   endComponent: React.ReactNode;
   middleComponent: React.ReactNode;
   className?: string;
+  startComponentContainerClassName?: string;
+  endComponentContainerClassName?: string;
+  middleComponentContainerClassName?: string;
+  component?: ContainerVariant;
+  startComponentElement?: ContainerVariant;
+  endComponentElement?: ContainerVariant;
+  middleComponentElement?: ContainerVariant;
 }
 const ThreeColumnFullSizeMid: React.FC<ThreeColumnFullSizeMidProps> = ({
   endComponent,
   middleComponent,
   startComponent,
   className,
+  middleComponentElement = 'div',
+  startComponentElement = 'div',
+  endComponentElement = 'div',
+  component = 'div',
+  endComponentContainerClassName,
+  middleComponentContainerClassName,
+  startComponentContainerClassName,
 }) => {
   return (
-    <ThreeColumnFullSizeMidComponent className={className}>
-      <div className="col-span-3 flex items-center">{startComponent}</div>
-      <div className="col-span-6 flex justify-center gap-[1rem]">
+    <VariableContainerComponent
+      component={component}
+      className={clsx('grid grid-cols-10', className)}
+    >
+      <VariableContainerComponent
+        component={startComponentElement}
+        className={clsx(
+          'col-span-3 flex items-center',
+          startComponentContainerClassName
+        )}
+      >
+        {startComponent}
+      </VariableContainerComponent>
+      <VariableContainerComponent
+        component={middleComponentElement}
+        className={clsx(
+          'col-span-6 flex justify-center gap-[1rem]',
+          middleComponentContainerClassName
+        )}
+      >
         {middleComponent}
-      </div>
-      <div className="col-span-1 flex items-center justify-end">
+      </VariableContainerComponent>
+      <VariableContainerComponent
+        component={endComponentElement}
+        className={clsx(
+          'col-span-1 flex items-center justify-end',
+          endComponentContainerClassName
+        )}
+      >
         {endComponent}
-      </div>
-    </ThreeColumnFullSizeMidComponent>
+      </VariableContainerComponent>
+    </VariableContainerComponent>
   );
 };
 
